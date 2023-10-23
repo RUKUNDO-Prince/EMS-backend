38e3f666e273d788fbc7719fbda30ff4467ab73a
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-10-23 07:50:53 +0300

    variables in java

--- src/variables/Variables.java
+++ src/variables/Variables.java
@@ -2,7 +2,9 @@ package variables;
 
 public class Variables {
     public static void main (String[] args) {
-        int age = 30;
-        System.out.println(age);
+//        int age = 30, weight = 75;
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println(herAge);
     }
 }
