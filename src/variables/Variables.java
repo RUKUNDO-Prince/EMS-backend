fb38dce47a23badb9fdf1674d3016cc06db42be7
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-10-23 08:00:57 +0300

    variables and datatypes in java

--- src/variables/Variables.java
+++ src/variables/Variables.java
@@ -3,8 +3,13 @@ package variables;
 public class Variables {
     public static void main (String[] args) {
 //        int age = 30, weight = 75;
-        int myAge = 30;
-        int herAge = myAge;
+        byte myAge = 30;
+        byte herAge = myAge;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        String name = "Prince";
+        boolean isEligible = true;
         System.out.println(herAge);
     }
 }
