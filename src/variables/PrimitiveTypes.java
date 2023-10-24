4d1d9b884ce757add8651fe568e7ba92d661c35f
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-10-24 07:25:36 +0300

    reference data types in java

---
+++ src/variables/PrimitiveTypes.java
@@ -0,0 +1,12 @@
+package variables;
+
+import java.util.Date;
+
+public class PrimitiveTypes {
+    public static void main (String[] args) {
+        byte age = 30;
+        Date now = new Date();
+//        now.getTime();
+        System.out.println(now);
+    }
+}
