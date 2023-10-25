568cd4db4c38b3116877df5d7485bd7a7979f368
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-10-25 07:24:46 +0300

    arrays

---
+++ src/variables/ArraysTypes.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class ArraysTypes {
+    public static void main (String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5}; // One dimensional arrays
+        System.out.println(Arrays.toString(numbers));
+    }
+}
