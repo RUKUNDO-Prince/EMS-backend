5f3cf39d96e3736d34b17eba96a8808177c461b8
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-10-25 07:31:26 +0300

    constants

---
+++ src/variables/Constants.java
@@ -0,0 +1,9 @@
+package variables;
+
+public class Constants {
+    public static void main (String[] args) {
+        final float Pi = 3.142F;
+        // Pi = 4; // Can't change its value
+        System.out.println(Pi);
+    }
+}
