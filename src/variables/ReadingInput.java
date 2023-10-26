da3d543746e2f8da237811982429b37eca144382
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-10-26 07:40:32 +0300

    reading input from user

--- src/variables/ReadingInput.java
+++ src/variables/ReadingInput.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class ReadingInput {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Provide your name below");
+        String name =  scanner.nextLine().trim();
+        System.out.println("Provide your age below");
         byte age = scanner.nextByte();
-        System.out.println("You are " + age + " years old");
+        System.out.println("Your name is " + name + " and you are " + age + " years old");
     }
 }
