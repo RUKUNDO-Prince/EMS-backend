2fce774de33bb1be8a91d2851e07bace9e45937f
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-10-30 07:43:56 +0200

    mortgage calculator project

--- src/MortgageCalculatorProject/Main.java
+++ src/MortgageCalculatorProject/Main.java
@@ -1,5 +1,6 @@
 package MortgageCalculatorProject;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 import java.lang.Math;
 
@@ -21,6 +22,7 @@ public class Main {
         int n = period * 12;
 //        float mortgage = principle * ((r*Math.pow(1+r, n))/(Math.pow(1+r, n) - 1));
         float mortgage = (float)(principle * ((r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1)));
-        System.out.println("The Mortgage is: " + mortgage);
+        String formatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("The Mortgage is: " + formatted);
     }
 }
