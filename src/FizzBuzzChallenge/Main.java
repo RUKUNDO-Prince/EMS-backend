b15d45b2994492fd1097d4853f4f0890b8bb5c11
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-11-01 07:37:01 +0200

    the fizzbuzz challenge

--- src/FizzBuzzChallenge/Main.java
+++ src/FizzBuzzChallenge/Main.java
@@ -10,9 +10,9 @@ public class Main {
         if (number % 3 == 0 && number % 5 == 0) {
             System.out.println("FizzBuzz");
         } else if (number % 5 == 0) {
-            System.out.println("Buzz");
-        } else if (number % 3 == 0) {
             System.out.println("Fizz");
+        } else if (number % 3 == 0) {
+            System.out.println("Buzz");
         } else {
             System.out.println(number);
         }
