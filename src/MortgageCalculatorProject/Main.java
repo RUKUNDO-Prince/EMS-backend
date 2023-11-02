e717b9153123708f928d4c6413057f1905221441
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-11-02 09:48:42 +0200

    updating the mortgage calculator

--- src/MortgageCalculatorProject/Main.java
+++ src/MortgageCalculatorProject/Main.java
@@ -6,10 +6,27 @@ import java.lang.Math;
 
 public class Main {
     public static void main (String[] args) {
+        long principle = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Mortgage Calculator!");
-        System.out.print("Principal: ");
-        long principle = scanner.nextLong();
+//        System.out.print("Principal ($1K - $1M): ");
+//        long inputValue = scanner.nextLong();
+//        if (inputValue < 1000 || inputValue > 1000000) {
+//            System.out.println("Enter a number between 1,000 and 1,000, 000");
+//        } else {
+//            principle = inputValue;
+//        }
+        while (true) {
+            System.out.print("Principal ($1K - $1M): ");
+            long inputValue = scanner.nextLong();
+            if (inputValue < 1000 || inputValue > 1000000) {
+                System.out.println("Enter a number between 1,000 and 1,000,000");
+            } else {
+                principle = inputValue;
+                break; // Exit the loop if a valid value is provided
+            }
+        }
+        System.out.println(principle);
         System.out.print("Annual interest rate: ");
         float rate = scanner.nextFloat();
         System.out.print("Period(years): ");
