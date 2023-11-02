53bb636f33e032caba5b2a59340c77b4d6924a48
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-11-02 09:00:46 +0200

    updating the mortgage calculator

--- src/MortgageCalculatorProject/Main.java
+++ src/MortgageCalculatorProject/Main.java
@@ -6,10 +6,6 @@ import java.lang.Math;
 
 public class Main {
     public static void main (String[] args) {
-//        long principal;
-//        float rate;
-//        int period;
-//        float mortgage;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Mortgage Calculator!");
         System.out.print("Principal: ");
@@ -20,7 +16,6 @@ public class Main {
         int period = scanner.nextInt();
         float r = (rate / 12)/100;
         int n = period * 12;
-//        float mortgage = principle * ((r*Math.pow(1+r, n))/(Math.pow(1+r, n) - 1));
         float mortgage = (float)(principle * ((r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1)));
         String formatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("The Mortgage is: " + formatted);
