9ba3306507894caa0e6c44b780daa6d363908e94
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-11-06 07:51:45 +0200

    methods

---
+++ src/Methods/Methods.java
@@ -0,0 +1,10 @@
+package Methods;
+
+public class Methods {
+    public static void main (String[] args) {
+        greetUser("Prince");
+    }
+    public static void greetUser (String name) {
+        System.out.println("Hello " + name);
+    }
+}
